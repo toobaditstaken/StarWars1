d7a0711894768c65d88d4859fe540d109d79c518
toobaditstaken <alexabbott10@yahoo.com>
2019-05-22 19:56:27 -0500

    changes

--- server1.js
+++ server1.js
@@ -1,63 +1,75 @@
 // Dependencies
-// ===========================================================
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Data
-// ===========================================================
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-}, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
-}];
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
 
 // Routes
-// ===========================================================
-
 app.get("/", function(req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-// What does this route do?
+// Displays all characters
 app.get("/api/characters", function(req, res) {
   return res.json(characters);
 });
 
-// What does this route do?
+// Displays a single character, or shows "No character found"
 app.get("/api/characters/:character", function(req, res) {
-  // What does this code do?
   var chosen = req.params.character;
+
   console.log(chosen);
 
-  // What does this code do?
   for (var i = 0; i < characters.length; i++) {
     if (chosen === characters[i].routeName) {
       return res.json(characters[i]);
     }
   }
 
-  // What does this code do?
   return res.send("No character found");
+
 });
 
-// Listener
-// ===========================================================
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  var newcharacter = req.body;
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
